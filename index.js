58c8d454030d083e1c434a13e0ad0a484c5416ff
litianzhao <tianzhao.li@bitmain.com>
2016-04-21 16:03:38 +0800

    fix: 修复异常没有捕获的问题

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// var babel = require('babel');
-
 var babel = require('babel-core');
-module.exports = function (content, file, options) {
-  var result = babel.transform(content, options);
-  return result.code; // 处理后的文件内容
-}
+module.exports = function(content, file, options) {
+    try {
+        var result = babel.transform(content, options);
+        return result.code; // 处理后的文件内容
+    } catch (e) {
+        return content;
+    }
+};
